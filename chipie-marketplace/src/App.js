090ad8c5a6593ddc76511871524cc6f7ac0b881d
daviidbartlett <daviidbartlett@gmail.com>
2021-11-17 12:18:31 +0000

    refactor to Routes and route syntax

--- chipie-marketplace/src/App.js
+++ chipie-marketplace/src/App.js
@@ -1,57 +1,37 @@
-import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Shop from './components/Shop';
-import UserList from './components/UserList';
-import Basket from './components/Basket';
-import ItemToSell from './components/ItemToSell';
-import React from 'react';
-import { UserContext } from './contexts/UserContext';
-import UserProfile from './components/UserProfile';
-import ShopCategory from './components/ShopCategory';
-import Home from './components/Home';
-import CreateUser from './components/CreateUser';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Shop from "./components/Shop";
+import UserList from "./components/UserList";
+import Basket from "./components/Basket";
+import ItemToSell from "./components/ItemToSell";
+import React from "react";
+import { UserContext } from "./contexts/UserContext";
+import UserProfile from "./components/UserProfile";
+import ShopCategory from "./components/ShopCategory";
+import Home from "./components/Home";
+import CreateUser from "./components/CreateUser";
 
 function App() {
-  const [user, setUser] = React.useState('Paul-R');
-
-  React.useEffect(() => {
-
-  }, [])
+  const [user, setUser] = React.useState("Paul-R");
 
   return (
     <div className="App">
-        <UserContext.Provider value={{ user, setUser}}>
-          <Header/>
-          <div className="content">
-            <Switch>
-            <Route exact path='/shop/:category'>
-                <ShopCategory />
-              </Route>
-              <Route path='/shop'>
-                <Shop/>
-              </Route>
-              <Route path="/users">
-                <UserList/>
-              </Route>
-              <Route path="/basket">
-                <Basket/>
-              </Route>
-              <Route path="/sell">
-                <ItemToSell />
-              </Route>
-              <Route path="/create-user">
-                  <CreateUser/>
-              </Route>
-              <Route path="/user-profile">
-                  <UserProfile />
-              </Route>
-              <Route path='/'>
-                <Home />
-              </Route>
-            </Switch>
-          </div>
-        </UserContext.Provider>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/shop/:category" element={<ShopCategory />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/users" element={<UserList />} />
+            <Route path="/basket" element={<Basket />} />
+            <Route path="/sell" element={<ItemToSell />} />
+            <Route path="/create-user" element={<CreateUser />} />
+            <Route path="/user-profile" element={<UserProfile />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </UserContext.Provider>
     </div>
   );
 }
